3690dbed8f1a7e0cac2927bde3af4b301178d73f
kiyun-kim <kiyn2403@gmail.com>
2026-03-24 23:01:38 +0900

    chore(platform): Ingress, ALB 생성 위해 잠시 주석처리..

--- envs/dev/platform/route53.tf
+++ envs/dev/platform/route53.tf
@@ -1,19 +1,19 @@
-data "aws_lb" "argocd" {
-  name = var.argocd_alb_name
+# data "aws_lb" "argocd" {
+#   name = var.argocd_alb_name
 
-  depends_on = [
-    kubernetes_ingress_v1.argocd
-  ]
-}
+#   depends_on = [
+#     kubernetes_ingress_v1.argocd
+#   ]
+# }
 
-resource "aws_route53_record" "argocd" {
-  zone_id = "Z09263044C7J602NPFIC"
-  name    = var.argocd_host
-  type    = "A"
+# resource "aws_route53_record" "argocd" {
+#   zone_id = "Z09263044C7J602NPFIC"
+#   name    = var.argocd_host
+#   type    = "A"
 
-  alias {
-    name                   = data.aws_lb.argocd.dns_name
-    zone_id                = data.aws_lb.argocd.zone_id
-    evaluate_target_health = true
-  }
-}
+#   alias {
+#     name                   = data.aws_lb.argocd.dns_name
+#     zone_id                = data.aws_lb.argocd.zone_id
+#     evaluate_target_health = true
+#   }
+# }
